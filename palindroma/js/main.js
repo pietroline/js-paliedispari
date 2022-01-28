ea62586a33065edadb6c460d082ff78f278ec442
pietroline <pietrobarberi@hotmail.it>
2022-01-28 16:18:02 +0100

    implementazione nel caso di parole con lunghezza dispari o inserimento di frasi (rotto riga 21)

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -4,14 +4,32 @@ function palindroma(argomento){
 
     let check = 0;
     
-    let stringa = argomento.toLowerCase();
+    //imposto tutti i caratteri in minuscolo, cosi evito l'errore nel caso in cui l'utente inserisca alcuni caratteri minuscoli e altri maiuscoli
+        let stringa = argomento.toLowerCase(); 
 
+        
+    //rimuovo tutti gli spazi, nel caso in cui venga in serita una frase
+        stringa = stringa.replaceAll(" ", "");
+        console.log(stringa);
+
+
+
+    /* verifico se la stringa inserita dall'utente è un array di caratteri pari o dispari
+    se l'array ha un numero di elementi pari tutti gli elementi saranno oggetto di confronto 
+    se l'array ha un numero di elementi dispari l'elemento centrale non sarà soggetto al confronto, lo elimino, non importa quale valore sia */
+        if(stringa % 2 != 0){ //caso dispari
+            // stringa.splice(stringa.lenght+1, 1);
+        }
+
+        console.log(stringa);
+    // confronto caratteri il primo con l'ultimo, il secondo con il penultimo, il terzo con il terzultimo ...
     for(let i=0; i<stringa.length/2; i++){
         if (stringa[i] == stringa[(stringa.length -1) - i]){
             check++;
         } 
     }
 
+    // se la stringa è palindroma significa che la prima metà degli elementi è uguale alla seconda meta degli elementi inversi
     if(check == argomento.length/2){
         alert(`"${argomento}" è palindroma` );
     }else{
